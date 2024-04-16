c4c4ca3abe4b6f653f9dc78f03db024a03ef8ed1
AMEE451 <desaiamee699@gmail.com>
2024-04-16 23:13:05 +0530

    Assignment1

---
+++ Lecture1/index1.js
@@ -0,0 +1,4 @@
+let name="Red and White"
+let tagline="A Transformation in Education"
+console.log(name,"\n",tagline);
+36
---
+++ Lecture1/index2.js
@@ -0,0 +1,4 @@
+let name="Amee"
+let age=18
+console.log(name,age);
+console.log(typeof name, typeof age);
