4c081faa662b74aac61ca3fc33b9072df66b9bcc
AMEE451 <desaiamee699@gmail.com>
2024-06-17 21:38:24 +0530

    Exam-6

--- Exam_6/Components/navbar.js
+++ Exam_6/Components/navbar.js
@@ -3,7 +3,7 @@ const navbar=()=>{
 
     return ` <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/exam_6/Pages/Home.html">Home</a>
+      <a class="navbar-brand" href="/exam_6/index.html">Home</a>
       <button
         class="navbar-toggler"
         type="button"
